d7c2c0aeed3ddbb7629e397be3c76da58a80b32b
fumi238000 <s21231128@gmail.com>
2020-11-28 00:45:26 +0900

    ハンディキャップの作成

--- user.rb
+++ user.rb
@@ -23,11 +23,11 @@ class User  < Player
 
       #必要な数値が入力されているか？
       break if (LIST_LENGTH).include?(select_num)
-      
+
       #エラーメッセージ
       not_select_kick_message
     end
-    
+
     #コースを変数に格納
     return kick_num = COURSE_LIST[select_num - 1]
 
@@ -35,24 +35,24 @@ class User  < Player
   #--------------------USERの守るコースの決定する--------------------
   def user_save_course(kick_count)
     while true
-      
+
       #GKであることを表示
       com_kicker_message(kick_count)
 
       # コース一覧の表示
       select_list
-      
+
       # USERが番号を選択する
       print "コースを数値で入力してください > "
       select_num = gets.chomp.to_i 
-      
+
       #必要な数値が入力されているか？
       break if (LIST_LENGTH).include?(select_num)
 
       #エラーメッセージ
         not_select_save_message
     end 
-    
+
     #コースを変数に格納
     return save_num  = COURSE_LIST[select_num - 1]
 
