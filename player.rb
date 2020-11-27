27f7a58653166eeec59416136778114a77f8c5ea
fumi238000 <s21231128@gmail.com>
2020-11-28 01:15:06 +0900

    調整

--- player.rb
+++ player.rb
@@ -12,11 +12,12 @@ class Player
   
   # コースリスト
   COURSE_LIST = [
+    #パターン１
     {num: "1", couse: RIGHT},
     {num: "2", couse: CENTER},
     {num: "3", couse: LEFT},
 
-    #以下追加する場合
+    #パターン２
     # {num: "1", couse: "真ん中"},
     # {num: "2", couse: "右上"},
     # {num: "3", couse: "右下"},
